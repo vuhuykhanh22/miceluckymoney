9f493447af4a647cc7fe4d1535e639be0291f32d
Khanh Vu <vuhuykhanh22@github.com>
2020-01-25 02:23:42 +0700

    Updates

---
+++ precache-manifest.721084e86a230798554a79f7a7db5197.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5471f0b2f454b2d9fe34e07f936346e",
+    "url": "/miceluckymoney/static/media/jerry.a5471f0b.png"
+  },
+  {
+    "revision": "d7974a3e7c5f238b9aa1",
+    "url": "/miceluckymoney/static/css/main.7c1406eb.chunk.css"
+  },
+  {
+    "revision": "0551237d4fa31913a465abf4d8e67a42",
+    "url": "/miceluckymoney/static/media/2020.0551237d.gif"
+  },
+  {
+    "revision": "77d41e2d19d866ef8ae6",
+    "url": "/miceluckymoney/static/js/1.77d41e2d.chunk.js"
+  },
+  {
+    "revision": "7b533410529f0ccdae1a",
+    "url": "/miceluckymoney/static/js/runtime~main.7b533410.js"
+  },
+  {
+    "revision": "39cbd09868cd4214b388c521e0287142",
+    "url": "/miceluckymoney/static/media/newyear-background.39cbd098.jpg"
+  },
+  {
+    "revision": "09e8e60f3f0bb399651553eb94e5489f",
+    "url": "/miceluckymoney/static/media/firework.09e8e60f.gif"
+  },
+  {
+    "revision": "331d8457118c2fe652e3819aa5309830",
+    "url": "/miceluckymoney/static/media/happy-1.331d8457.gif"
+  },
+  {
+    "revision": "7c5dccae829b2b572850ad24fe335118",
+    "url": "/miceluckymoney/static/media/happy-2.7c5dccae.gif"
+  },
+  {
+    "revision": "a907eab5e96994d77c7c240b086ecbb2",
+    "url": "/miceluckymoney/static/media/happy-3.a907eab5.gif"
+  },
+  {
+    "revision": "aaf542d004e6143833bc9a01098c36f1",
+    "url": "/miceluckymoney/static/media/happy-4.aaf542d0.gif"
+  },
+  {
+    "revision": "4d1b55ff27a2070d41f1a5fa87a19a23",
+    "url": "/miceluckymoney/static/media/gonzales.4d1b55ff.png"
+  },
+  {
+    "revision": "c60c0a6f45ad384aa71d9e71761fa260",
+    "url": "/miceluckymoney/static/media/gus.c60c0a6f.png"
+  },
+  {
+    "revision": "c39721f0b482c5e1857fbe6776e192b2",
+    "url": "/miceluckymoney/static/media/jag.c39721f0.png"
+  },
+  {
+    "revision": "d7974a3e7c5f238b9aa1",
+    "url": "/miceluckymoney/static/js/main.d7974a3e.chunk.js"
+  },
+  {
+    "revision": "f4d4b1403000880d6aa1de64003bf215",
+    "url": "/miceluckymoney/static/media/mickey.f4d4b140.png"
+  },
+  {
+    "revision": "5b88b66270ef7ffb391d4e5240c26693",
+    "url": "/miceluckymoney/static/media/minie.5b88b662.png"
+  },
+  {
+    "revision": "ab4af80bb61545afe0747d06fe787ea7",
+    "url": "/miceluckymoney/static/media/perla.ab4af80b.png"
+  },
+  {
+    "revision": "76f027c1d8ea2170cedd976017330331",
+    "url": "/miceluckymoney/static/media/pikachu.76f027c1.png"
+  },
+  {
+    "revision": "a3e0eb0fecb23627b1c1a2997856b57b",
+    "url": "/miceluckymoney/static/media/ratatouille.a3e0eb0f.png"
+  },
+  {
+    "revision": "30067f252f2a17109ed9544f00327ad6",
+    "url": "/miceluckymoney/static/media/rattata.30067f25.png"
+  },
+  {
+    "revision": "c66280e977ad6d706f07432dcd9c1cc9",
+    "url": "/miceluckymoney/static/media/rhino.c66280e9.png"
+  },
+  {
+    "revision": "f1aa8614963ad5bba6807d6c771e9ac9",
+    "url": "/miceluckymoney/static/media/stuart.f1aa8614.png"
+  },
+  {
+    "revision": "28d95d39981cfb5340f0c41a44168120",
+    "url": "/miceluckymoney/static/media/timothy.28d95d39.png"
+  },
+  {
+    "revision": "e75d7ab8590ef2f0afdfe2cbbd85e3e5",
+    "url": "/miceluckymoney/static/media/tuffy.e75d7ab8.png"
+  },
+  {
+    "revision": "184d1dd7684635320fb3bb73195be613",
+    "url": "/miceluckymoney/static/media/white.184d1dd7.png"
+  },
+  {
+    "revision": "77d41e2d19d866ef8ae6",
+    "url": "/miceluckymoney/static/css/1.b008c538.chunk.css"
+  },
+  {
+    "revision": "0e21ceac02cf0438bcb040d1c7a1353f",
+    "url": "/miceluckymoney/index.html"
+  }
+];
