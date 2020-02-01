2358238c336212144ec77ee792d592509a3fbe15
Khanh Vu <vuhuykhanh22@github.com>
2020-02-01 22:13:02 +0700

    Update

--- src/Component/LuckyMoney.js
+++ src/Component/LuckyMoney.js
@@ -68,7 +68,7 @@ export default class LuckyMoney extends Component {
                 : 'mice'} /></div>
         });
         return (
-            <div className='row ml-4'>
+            <div className='row'>
                 {images}
                 {this.state.show ?  
                     <div className = "ui-notice">  
